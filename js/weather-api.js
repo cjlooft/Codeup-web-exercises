487ce421b894635ab572ebdf7ea8d8229b478fda
c.josettelooft <cjlooft@gmail.com>
2018-09-04 13:35:24 -0500

    local weather information added

--- js/weather-api.js
+++ js/weather-api.js
@@ -1,27 +1,30 @@
 
 
-console.log("Reading js file");
-
-
+console.log("ok");
 
 
 $.getJSON("http://api.openweathermap.org/data/2.5/weather", {
-    APPID: "var apiKey = localStorage.getItem(\"owmApiKey\");",
+    APPID: "29a78bdd314f87e8cfe1faa62ca1cf45",
     lat: 29.4230,
-    lon: -98.48527
+    lon: -98.48527,
+    units: "imperial"
 
 }).done(function(data) {
-    $("#cityName").html("");
-   // data.forEach(function(data) {//here foreach is not being called right
+    console.log(data);
 
 
         var html = "<div>";
         html += "<h2>" + data.name + "</h2>";
-        html += "<h2>" + data.main.temp_max +  "/" + data.main.temp_min+"</h2>";
-        html += "<h2>" + data.weather.icon + "</h2>";
-        html += "<h4> Wind Speed " + data.wind.speed + "</h4>";
-        html += "<h4>" + data.rain + "</h4>";
-        html += "<h4>" + data.clouds.all + "</h4>";
-        html += "<h4> Humidity " + data.main.humidity + "</h4>";
-        $("#cityName").append(html);
-    });
+        html += "<h2>" + data.main.temp_max.toFixed(0) +  "° / " + data.main.temp_min.toFixed(0) + "°</h2>";
+        html += "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'/>";
+    // html += "<h4>" + data..weather[0].description + "</h4>";
+
+
+    html += `<h4>${data.weather[0].description}</h4>`;
+        html += "<h4> Humidity: " + data.main.humidity + "% </h4>";
+        html += "<h4> Wind: "+ data.wind.deg + " deg. " + data.wind.speed.toFixed(0) + "mph" + "</h4>";
+    html += "<h4> Pressure: "  + data.main.pressure + " millibars</h4>";
+    $("#cityName").append(html);
+
+//
+});
